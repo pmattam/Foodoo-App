e6357340c724753ea6e69b97330ae90d56046738
Jaehee Kim <jaeheekim051510@gmail.com>
2018-05-01 12:31:00 -0400

    header added to each components

--- src/Screens/OneEventScreen.js
+++ src/Screens/OneEventScreen.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import EventContainer from '../Components/EventContainer';
 import NavBack from '../Components/NavBack';
+import Header from '../Components/Header'
 
 let OneEventScreen = () => {
     return (
       <div className="OneEventScreen">
+      <header>
+        <Header />
+      </header>
         <p>The Banner Component rendered</p>
         < EventContainer />
         < NavBack />
