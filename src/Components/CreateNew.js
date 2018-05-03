db092486834f8c5c31dc3fb0810557e1600dc8c7
janelleany <janellealexander12@gmail.com>
2018-05-03 15:52:16 -0400

    "part 2" of new event form created

--- src/Components/CreateNew.js
+++ src/Components/CreateNew.js
@@ -8,71 +8,88 @@ let mapDispatchToProps = (dispatch) => {
   return {newEventCreator: (details) => dispatch(newEventCreator(details))};
 }
 
-
-
 class CreateNew extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      creatingPartOne: true
+    }
   }
 
     render() {
-      return (
-        <div className="form-container">
 
-          <div className="client-name">
-            <p> Client Name </p>
-            <input type="text" name="client-name-input" onChange={(event) => this.setState({clientname: event.target.value})}/>
-          </div>
+      let clickNext = () => {
+        return this.setState({creatingPartOne: false});
+      }
+      
+      let clickSave = () => {
+        return this.props.newEventCreator(this.state);
+      }
 
-          <div className="date">
-            <p> Date </p>
-            <input type="text" name="date-input" onChange={(event) => this.setState({eventdate: event.target.value})}/>
-          </div>
+      if (this.state.creatingPartOne) {
+        return (
+          <div className="form-container">
 
-          <div className="event-title">
-            <p> Event Title </p>
-            <input type="text" name="event-title-input" onChange={(event) => this.setState({eventtitle: event.target.value})}/>
-          </div>
+            <div className="client-name">
+              <p> Client Name </p>
+              <input type="text" name="client-name-input" onChange={(event) => this.setState({clientname: event.target.value})}/>
+            </div>
 
-          <div className="event-description">
-            <p> Event Description </p>
-            <textarea name="event-description-input" onChange={(event) => this.setState({eventdescription: event.target.value})}/>
+            <div className="date">
+              <p> Date </p>
+              <input type="text" name="date-input" onChange={(event) => this.setState({eventdate: event.target.value})}/>
+            </div>
+
+            <div className="event-title">
+              <p> Event Title </p>
+              <input type="text" name="event-title-input" onChange={(event) => this.setState({eventtitle: event.target.value})}/>
+            </div>
+
+            <div className="event-description">
+              <p> Event Description </p>
+              <textarea name="event-description-input" onChange={(event) => this.setState({eventdescription: event.target.value})}/>
+            </div>
+    
+    
+            <div className="event-type">
+              <p> Event Type </p>  
+              <label className="event-type-dropdown">
+                <select className="event-type" onChange={(event) => this.setState({eventtype: event.target.value})}>
+                  <option default>Select</option>
+                  <option value="Wedding">Wedding</option>
+                  <option value="Graduation">Graduation</option>
+                  <option value="Corporate">Corporate</option>
+                  <option value="Anniversary">Anniversary</option>
+                  <option value="Birthday">Birthday</option>
+                </select>
+              </label>
+            </div>
+    
+            <div className="number-of-guests">
+              <p> Number of Guests </p>
+              <input type="number" name="number-of-guests-input" onChange={(event) => this.setState({eventsize: event.target.value})}/>
+            </div>
+    
+            <div className="meal-type">
+              <p> Meal Type </p>
+              <label>
+                <select className="meal-type-drop-down" onChange={(event) => this.setState({mealtype: event.target.value})}>
+                  <option default>Select</option>
+                  <option value="Buffet">Buffet</option>
+                  <option value="Sit-down">Sit Down</option>
+                </select>
+              </label>
+            </div>
+            <button onClick={clickNext}>Next =></button>
+        </div>
+        )
+      } else {
+        return (
+          <div>
+            <h3>Menu Details</h3>
           </div>
-  
-  
-          <div className="event-type">
-            <p> Event Type </p>  
-            <label className="event-type-dropdown">
-              <select className="event-type" onChange={(event) => this.setState({eventtype: event.target.value})}>
-                <option default>Select</option>
-                <option value="Wedding">Wedding</option>
-                <option value="Graduation">Graduation</option>
-                <option value="Corporate">Corporate</option>
-                <option value="Anniversary">Anniversary</option>
-                <option value="Birthday">Birthday</option>
-              </select>
-            </label>
-          </div>
-  
-          <div className="number-of-guests">
-            <p> Number of Guests </p>
-            <input type="number" name="number-of-guests-input" onChange={(event) => this.setState({eventsize: event.target.value})}/>
-          </div>
-  
-          <div className="meal-type">
-            <p> Meal Type </p>
-            <label>
-              <select className="meal-type-drop-down" onChange={(event) => this.setState({mealtype: event.target.value})}>
-                <option default>Select</option>
-                <option value="Buffet">Buffet</option>
-                <option value="Sit-down">Sit Down</option>
-              </select>
-            </label>
-          </div>
-          <button onClick={ () => this.props.newEventCreator(this.state) }>Save</button>
-      </div>
-      )
+        )
+      }
     }  
 }
 
