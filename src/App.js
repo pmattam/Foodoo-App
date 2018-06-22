ba743bcce4cb87997946b67a7c0f6f87387eee36
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-06-21 22:11:28 -0400

    Checking isLoggedIn and only if true then getting all the events

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import LoginScreen from './Screens/LoginScreen';
 import AllEventsScreen from './Screens/AllEventsScreen';
@@ -14,16 +14,16 @@ import LogoutScreen from './Screens/LogoutScreen';
 const App = () => {
   return (
     <Router>
-      <div>
-        <Route path="/" exact component={LoginScreen}/>
-        <Route path="/events" exact component={AllEventsScreen}/>
-        <Route path="/events/:id" component={OneEventScreen}/>
-        <Route path="/new" component={CreateNewScreen}/>
-        <Route path="/shoppinglist/:id" component={ShoppingListScreen}/>
-        <Route path="/register" component={RegisterScreen}/>
-        <Route path="/logout" component={LogoutScreen}/>
-      </div>
+        <Switch>
+          <Route path="/" exact component={LoginScreen}/>
+          <Route path="/register" component={RegisterScreen}/>
+          <Route path="/logout" component={LogoutScreen}/>
+          <Route path="/events/:id" component={OneEventScreen}/>
+          <Route path="/events" exact component={AllEventsScreen}/>
+          <Route path="/new" component={CreateNewScreen}/>
+          <Route path="/shoppinglist/:id" component={ShoppingListScreen}/>
+        </Switch>
     </Router>
   )
-}
+};
 export default App;
